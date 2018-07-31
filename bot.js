038220c5e7c530e1b4a1464041ff85ca644049f9
FASIBOT <41910419+FASIBOT@users.noreply.github.com>
2018-07-31 16:31:11 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === '/twitch') {
+    	message.reply('https://www.twitch.tv/benjiceleven');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
