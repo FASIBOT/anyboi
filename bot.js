0e5b709252472ee5c8dee4a3a388b62db09bdee7
FASIBOT <41910419+FASIBOT@users.noreply.github.com>
2018-07-31 16:41:45 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,26 +6,20 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '/creator') {
+    if (message.content === '.creator') {
     	message.reply('BenjiCelven, he has a Twitch channel, and a Youtube one! Use "/twitch" and "/yt" for them.');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === '/yt') {
+    if (message.content === '.yt') {
     	message.reply('https://www.youtube.com/channel/UC7fHF0SUMGd9x32odZ9hv_A');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === '/twitch') {
+    if (message.content === '.twitch') {
     	message.reply('https://www.twitch.tv/benjiceleven');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === '/insult') {
-    	message.reply('*you have no friends.*');
   	}
 });
 
