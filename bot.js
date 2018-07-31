9553697acc6f482e80f318ef3afdf988a59feff4
FASIBOT <41910419+FASIBOT@users.noreply.github.com>
2018-07-31 16:27:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,11 +23,6 @@ client.on('message', message => {
   	}
 });
 
-client.on('message', message => {
-    if (message.content === '/insult') {
-    	message.reply('has no friends. *They are lonely. Nobody loves them.* Lol jk.');
-  	}
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
